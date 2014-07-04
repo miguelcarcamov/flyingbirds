ab7c024c27b3fcb654950779fc106fda81175b83
SmokeInBrain <miguel.carcamo@usach.cl>
2014-07-04 01:54:42 -0400

    get opt fixed

--- flyingbirds/src/main.cpp
+++ flyingbirds/src/main.cpp
@@ -1,12 +1,14 @@
 #include <uC++.h>
 #include "graphics/Graphics.h"
+#include "getOptions/getOptions.h"
 
 
 using namespace std;
 
 void uMain::main() {
 
-	getOptions(argc, argv);
+	getOptions getoptions = getOptions();
+	getoptions.getOptions(argc, argv);
     Graphics graphics = Graphics();
     graphics.initGraphics();
     
